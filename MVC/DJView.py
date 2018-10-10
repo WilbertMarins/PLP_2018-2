160e196c9cba468fe1d2673d436cf48ff0ec875b
Wilbert Marins <wilbertluis2014@gmail.com>
2018-10-10 12:14:41 -0400

    Add files via upload

---
+++ MVC/DJView.py
@@ -0,0 +1,41 @@
+def interface():
+	'''
+ 	BeatModelInterface
+	ControllerInterface
+	beatBar
+  	setBPMButton
+	increaseBPMButton
+	decreaseBPMButton
+  	menuBar
+  	MenuItem:startMenuItem
+      	MenuItem:stopMenuItem
+	'''
+
+def DJView():
+	#visualização das interfaces
+	#ControllerInterface
+	#BeatModelInterface
+    
+def createView():
+	#cria a tela
+  
+def createControls():
+	#cria os botões funcionais na tela
+        
+def enableStopMenuItem():
+	#habilita
+	
+def disableStopMenuItem():
+    	#desabilita
+
+def enableStartMenuItem():
+    	#habilita	
+
+def disableStartMenuItem():
+    	#desabilita
+	
+def updateBPM():
+	#atualiza o BPM
+	
+def updateBeat():
+	#atualiza a sequencia de beats
