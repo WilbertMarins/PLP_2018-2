d135d98be366e75c7f50c9c76ea5709931e7a121
Wilbert Marins <wilbertluis2014@gmail.com>
2018-10-10 11:07:09 -0400

    Create BeatModel.py

---
+++ BeatModel.py
@@ -0,0 +1,30 @@
+BPM = 90      #valor padrão
+
+#inicia a aplicação
+def initialize():
+  
+  
+		
+#liga
+def on():
+		print("Iniciando a sequência") #imprime a mensagem		
+		BPM=90
+    #inicia sequencia
+	
+#desliga
+def off():
+		BPM = 0
+		#para sequencia
+	
+#altera o Bpm recebendo novo valor
+def recebeBPM(bpm): 
+		BPM = bpm
+    
+    
+#atualiza o modelo
+def beatEvent():
+    #atualiza em tela
+		
+	
+
+
